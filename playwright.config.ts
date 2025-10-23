e262293bb9e90216d9b75fe549d260c2a7d717fa
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-10-23 16:16:34 +1100

    test: added the test

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,28 +2,26 @@ import { defineConfig, devices } from '@playwright/test';
 
 const testEnv = {
   NODE_ENV: 'test',
-  PORT: '3000',
-  SESSION_SECRET: 'test-session-secret-for-e2e-tests-only',
-  SESSION_DURATION: '3600',
-  ZITADEL_DOMAIN: 'https://test.zitadel.cloud',
-  ZITADEL_CLIENT_ID: 'test-client-id',
-  ZITADEL_CLIENT_SECRET: 'test-client-secret',
-  ZITADEL_CALLBACK_URL: 'http://localhost:3000/api/auth/callback',
-  ZITADEL_POST_LOGOUT_URL: 'http://localhost:3000/api/auth/logout/callback',
-  AUTH_URL: 'http://localhost:3000',
-  PUBLIC_API_URL: 'http://localhost:3000',
+  VITE_PORT: '3000',
+  VITE_SESSION_SECRET: 'test-session-secret-for-e2e-tests-only',
+  VITE_SESSION_DURATION: '3600',
+  VITE_ZITADEL_DOMAIN: 'https://test.zitadel.cloud',
+  VITE_ZITADEL_CLIENT_ID: 'test-client-id',
+  VITE_ZITADEL_CLIENT_SECRET: 'test-client-secret',
+  VITE_ZITADEL_CALLBACK_URL: 'http://localhost:3000/auth/callback',
+  VITE_ZITADEL_POST_LOGOUT_URL:
+    'http://localhost:3000/api/auth/logout/callback',
 };
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   testDir: './test',
-  outputDir: './build/playwright',
+  outputDir: './build/test-results',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
-    ['html', { outputFolder: './build/playwright-report' }],
     ['list'],
     [
       'junit',
